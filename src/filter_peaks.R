4b3b59369c038abea54453363dc7d05a5e84c177
Polina Guseva <p.guseva00@gmail.com>
2021-06-06 15:45:28 +0300

    fixed bug with incorrect threshold

--- src/filter_peaks.R
+++ src/filter_peaks.R
@@ -24,8 +24,8 @@ ggsave(paste0('filter_peaks.', NAME, '.init.hist.pdf'), path = OUT_DIR)
 # Remove long peaks
 bed_df <- bed_df %>%
   arrange(-len) %>%
-  filter(len < 5000)
-  
+  filter(len < THRESHOLD)
+
 ggplot(bed_df) +
   aes(x = len) +
   geom_histogram() +
