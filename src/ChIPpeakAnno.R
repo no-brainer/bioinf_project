e06a139ce2ede39fd63c4f1ebd6a899cdddf3e0f
Polina Guseva <p.guseva00@gmail.com>
2021-06-06 19:02:55 +0300

    gene association

---
+++ src/ChIPpeakAnno.R
@@ -0,0 +1,37 @@
+source('lib.R')
+
+###
+
+library(ChIPpeakAnno)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(org.Hs.eg.db)
+#library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+#library(org.Mm.eg.db)
+
+###
+
+NAME <- 'H3K4me3_H9.intersect_with_G4_seq_Li_K'
+
+peaks <- toGRanges(paste0(DATA_DIR, NAME, '.bed'), format="BED")
+peaks[1:2]
+
+annoData <- toGRanges(TxDb.Hsapiens.UCSC.hg19.knownGene)
+annoData[1:2]
+
+
+anno <- annotatePeakInBatch(peaks, AnnotationData=annoData,
+                            output="overlapping",
+                            FeatureLocForDistance="TSS",
+                            bindingRegion=c(-2000, 300))
+data.frame(anno) %>% head()
+
+anno$symbol <- xget(anno$feature, org.Hs.egSYMBOL)
+data.frame(anno) %>% head()
+
+anno_df <- data.frame(anno)
+write.table(anno_df, file=paste0(DATA_DIR, NAME, '.genes.txt'),
+            col.names = TRUE, row.names = FALSE, sep = '\t', quote = FALSE)
+
+uniq_genes_df <- unique(anno_df['symbol'])
+write.table(uniq_genes_df, file=paste0(DATA_DIR, NAME, '.genes_uniq.txt'),
+            col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
