31f80a4bfef31a48347e7a8bbdf0e3f96df7c143
Polina Guseva <p.guseva00@gmail.com>
2021-06-06 17:49:35 +0300

    distribution plot for intersection lengths

--- src/len_hist.R
+++ src/len_hist.R
@@ -4,7 +4,8 @@ source('lib.R')
 #NAME <- 'H3K4me3_H9.ENCFF464FFZ.hg38'
 #NAME <- 'H3K4me3_H9.ENCFF473AUA.hg19'
 #NAME <- 'H3K4me3_H9.ENCFF473AUA.hg38'
-NAME <- 'G4_seq_Li_K'
+#NAME <- 'G4_seq_Li_K'
+NAME <- 'H3K4me3_H9.intersect_with_G4_seq_Li_K'
 
 bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
 #colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
